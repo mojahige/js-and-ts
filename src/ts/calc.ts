350459ddb1434d06cd2249af09817aaf82669923
mojahige <mojahige@gmail.com>
2019-10-23 12:19:22 +0900

    feat: 新しいダメな関数追加

--- src/ts/calc.ts
+++ src/ts/calc.ts
@@ -5,3 +5,5 @@ export const addition = (x: number, y: number): number => x + y;
 // 型定義を出力して見たら hoge の return は any
 // (allowJs の時点で怒られるけど、一応生成はされた)
 export const fuga = (x: number) => hoge(x);
+
+export const dame = () => addition(1, '');
