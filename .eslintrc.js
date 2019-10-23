2f58aa8bc9576537dfb1bbb4429c531b508c4b31
Yu Nozato <nozato.yu@hamee.co.jp>
2019-10-23 11:44:04 +0900

    refactor: --print-config 見つつ整理整頓

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,24 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {},
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      rules: {},
+    },
+  ],
 };
