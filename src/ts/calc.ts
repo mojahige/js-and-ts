cc0e76b4dcb38c7cf0b2ac56c4dd53419f1feec3
Yu Nozato <nozato.yu@hamee.co.jp>
2019-10-23 11:45:52 +0900

    chore: add comment

--- src/ts/calc.ts
+++ src/ts/calc.ts
@@ -2,4 +2,6 @@ import { hoge } from '../index';
 
 export const addition = (x: number, y: number) => x + y;
 
+// 型定義を出力して見たら hoge の return は any
+// (allowJs の時点で怒られるけど、一応生成はされた)
 export const fuga = (x: number) => hoge(x);
